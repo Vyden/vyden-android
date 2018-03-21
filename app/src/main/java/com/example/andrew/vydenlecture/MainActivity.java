6c5e2436f36ba015115713f134f6f3980e65a45d
Andrew Arpasi <andrew@andrewarpasi.com>
2018-03-20 22:50:38 -0400

    Initial commit - working video playback

---
+++ app/src/main/java/com/example/andrew/vydenlecture/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.andrew.vydenlecture;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Remove title bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        try {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e) {
+        }
+
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //set content view AFTER ABOVE sequence (to avoid crash)
+        this.setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webview);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setMediaPlaybackRequiresUserGesture(false);
+
+        webView.loadUrl("http://andrewarpasi.com/vyden/?course=-L62Ur6nLSp470r1Iyk6&lecture=-L62V9b_SmlELZIZ8k71");
+
+    }
+
+
+}
